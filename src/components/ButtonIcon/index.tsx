b1367d2fec121e6358582a9120f836f84fe10575
wdrik <w.drik@outlook.com>
2021-06-21 12:58:29 -0300

    chore: add prettier

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+
+import DiscordImg from '../../assets/discord.png';
+
+import { styles } from './styles';
+
+export function ButtonIcon() {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} resizeMode="stretch" />
+      </View>
+
+      <Text style={styles.title}>Entrar com Discord</Text>
+    </TouchableOpacity>
+  );
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { Text, View, Image, StatusBar } from 'react-native';
 
-import { styles } from "./styles";
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!!!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {`\n`} e organize suas {`\n`} jogatinas
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon />
+      </View>
     </View>
   );
 }
