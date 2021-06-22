752e09d17f7637b2d57c2b394a86adb86c09073e
wdrik <w.drik@outlook.com>
2021-06-22 16:33:31 -0300

    feat: add avatar component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,9 +3,12 @@ import { View, Text } from 'react-native';
 
 import { styles } from './styles';
 
+import { Avatar } from '../Avatar';
+
 export function Profile() {
   return (
     <View style={styles.container}>
+      <Avatar urlImage={'https://github.com/wdrik.png'} />
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá</Text>
