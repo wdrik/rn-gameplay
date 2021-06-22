189327ac575653347fc29c159a32900cfc5ae564
wdrik <w.drik@outlook.com>
2021-06-22 14:26:42 -0300

    feat: add profile component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import { Profile } from '../../components/Profile';
+
 import { styles } from './styles';
 
 export function Home() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}></View>
+    <View>
+      <View style={styles.header}>
+        <Profile />
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,11 +2,6 @@ import { StyleSheet } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   header: {
     width: '100%',
     paddingHorizontal: 24,
