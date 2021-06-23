1aa1113074eaffc502409a98f2c14bee4ef03171
wdrik <w.drik@outlook.com>
2021-06-23 16:42:22 -0300

    fix: resolve transition bugs

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -7,6 +7,7 @@ import { CategorySelect } from '../../components/CategorySelect';
 import { ListHeader } from '../../components/ListHeader';
 import { Appointment } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
+import { Background } from '../../components/Background';
 
 import { styles } from './styles';
 
@@ -47,7 +48,7 @@ export function Home() {
   }
 
   return (
-    <View>
+    <Background>
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
@@ -70,6 +71,6 @@ export function Home() {
           ItemSeparatorComponent={() => <ListDivider />}
         />
       </View>
-    </View>
+    </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -7,6 +7,8 @@ import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
+import { Background } from '../../components/Background';
+
 export function SignIn() {
   const navigation = useNavigation();
 
@@ -15,25 +17,27 @@ export function SignIn() {
   }
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={IllustrationImg}
-        style={styles.image}
-        resizeMode="stretch"
-      />
+    <Background>
+      <View style={styles.container}>
+        <Image
+          source={IllustrationImg}
+          style={styles.image}
+          resizeMode="stretch"
+        />
 
-      <View style={styles.content}>
-        <Text style={styles.title}>
-          Conecte-se {'\n'} e organize suas {'\n'} jogatinas
-        </Text>
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Conecte-se {'\n'} e organize suas {'\n'} jogatinas
+          </Text>
 
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {'\n'}
-          favoritos com seus amigos
-        </Text>
+          <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {'\n'}
+            favoritos com seus amigos
+          </Text>
 
-        <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} />
+          <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} />
+        </View>
       </View>
-    </View>
+    </Background>
   );
 }
