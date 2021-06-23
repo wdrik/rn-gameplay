a8fd5e236f677dde293837cb5518bb4a4050bed4
wdrik <w.drik@outlook.com>
2021-06-22 23:14:29 -0300

    feat: add category component

--- src/components/ButtonAdd/index.tsx
+++ src/components/ButtonAdd/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
+import { CategorySelect } from '../../components/CategorySelect';
 
 import { styles } from './styles';
 
@@ -12,6 +13,10 @@ export function Home() {
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect />
       </View>
     </View>
   );
