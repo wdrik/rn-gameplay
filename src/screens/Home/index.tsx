f81c327c999baba092d7d3774c304880dd37a19f
wdrik <w.drik@outlook.com>
2021-06-23 09:13:26 -0300

    feat: add select category feature

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 
 import { Profile } from '../../components/Profile';
@@ -8,6 +8,12 @@ import { CategorySelect } from '../../components/CategorySelect';
 import { styles } from './styles';
 
 export function Home() {
+  const [category, setCategory] = useState('');
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View>
       <View style={styles.header}>
@@ -16,7 +22,10 @@ export function Home() {
       </View>
 
       <View>
-        <CategorySelect />
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
       </View>
     </View>
   );
