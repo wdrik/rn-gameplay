9dbc7cf64fb9dc770996540c542c8d0e3dd97254
wdrik <w.drik@outlook.com>
2021-06-24 14:50:18 -0300

    feat: add member component

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, ImageBackground, View, FlatList } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
@@ -7,9 +7,30 @@ import { styles } from './styles';
 
 import { Background } from '../../components/Background';
 import { Header } from '../../components/Header';
+import { ListHeader } from '../../components/ListHeader';
+import { Member } from '../../components/Member';
+import { ListDivider } from '../../components/ListDivider';
+
 import { theme } from '../../global/styles/theme';
 
+import BannerImg from '../../assets/banner.png';
+
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: '1',
+      username: 'Iorgen',
+      avatar_url: 'https://github.com/wdrik.png',
+      status: 'online',
+    },
+    {
+      id: '2',
+      username: 'Wildrik',
+      avatar_url: 'https://github.com/wdrik.png',
+      status: 'offline',
+    },
+  ];
+
   return (
     <Background>
       <Header
@@ -21,7 +42,24 @@ export function AppointmentDetails() {
         }
       />
 
-      <View style={styles.container}></View>
+      <ImageBackground source={BannerImg} style={styles.banner}>
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>Lendários</Text>
+          <Text style={styles.subtitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+          </Text>
+        </View>
+      </ImageBackground>
+
+      <ListHeader title="Jogadores" subtitle="Total 3" />
+
+      <FlatList
+        data={members}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Member data={item} />}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.members}
+      />
     </Background>
   );
 }
--- src/screens/AppointmentDetails/styles.ts
+++ src/screens/AppointmentDetails/styles.ts
@@ -1,5 +1,31 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
   container: {},
+  banner: {
+    width: '100%',
+    height: 234,
+    marginBottom: 30,
+  },
+  bannerContent: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 24,
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: theme.fonts.title700,
+    color: theme.colors.heading,
+  },
+  subtitle: {
+    fontSize: 13,
+    fontFamily: theme.fonts.text400,
+    color: theme.colors.heading,
+  },
+  members: {
+    marginLeft: 24,
+    marginRight: 27,
+  },
 });
