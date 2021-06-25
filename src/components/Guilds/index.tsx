e7db73b6d99ff9c3dfbba0800c39bf43f974ab78
wdrik <w.drik@outlook.com>
2021-06-25 08:55:33 -0300

    feat: add guild components

---
+++ src/components/Guilds/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+
+import { styles } from './styles';
+
+export function Guilds() {
+  const guilds = [
+    {
+      id: '1',
+      name: 'Lendários',
+      icon: null,
+      owner: true,
+    },
+    {
+      id: '2',
+      name: 'Lendários',
+      icon: null,
+      owner: true,
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <FlatList data={guilds} keyExtractor={(item) => item.id} renderItem={({item}) => (
+        
+      )} />
+    </View>
+  );
+}
