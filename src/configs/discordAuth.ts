3b5e9cf62da98390d2c81ab7aedb61eaae27d9c1
wdrik <w.drik@outlook.com>
2021-06-26 22:00:51 -0300

    feat: persisting authenticated user data

--- src/configs/discordAuth.ts
+++
@@ -1,7 +0,0 @@
-const REDIRECT_URI = 'https://auth.expo.io/@wdrik/gameplay';
-const SCOPE = 'identify%20email%20connections%20guilds';
-const RESPONSE_TYPE = 'token';
-const CLIENT_ID = '858091925386690571';
-const CDN_IMAGE = 'https://cdn.discordapp.com';
-
-export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
