8e4ef8571c3c7ba37db44d02058546d17d5beb60
wdrik <w.drik@outlook.com>
2021-06-27 13:56:29 -0300

    feat: load appointments

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,49 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { View, FlatList } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
 import { ListHeader } from '../../components/ListHeader';
-import { Appointment } from '../../components/Appointment';
+import { Appointment, AppointmentProps } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
 import { Background } from '../../components/Background';
+import { Load } from '../../components/Load';
 
 import { styles } from './styles';
+import { COLLECTION_APPOINTMENTS } from '../../configs/database';
 
 export function Home() {
   const [category, setCategory] = useState('');
-  const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
+  const [appointments, setAppointments] = useState<AppointmentProps[]>([]);
 
-  const appointments = [
-    {
-      id: '1',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: true,
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description:
-        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
-    },
-    {
-      id: '2',
-      guild: {
-        id: '1',
-        name: 'Lendários',
-        icon: null,
-        owner: true,
-      },
-      category: '1',
-      date: '22/06 às 20:40h',
-      description:
-        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
-    },
-  ];
+  const navigation = useNavigation();
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -57,6 +34,25 @@ export function Home() {
     navigation.navigate('AppointmentCreate');
   }
 
+  async function loadAppointments() {
+    const response = await AsyncStorage.getItem(COLLECTION_APPOINTMENTS);
+    const storage: AppointmentProps[] = response ? JSON.parse(response) : [];
+
+    if (category) {
+      setAppointments(storage.filter((item) => item.category === category));
+    } else {
+      setAppointments(storage);
+    }
+
+    setLoading(false);
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      loadAppointments();
+    }, [category])
+  );
+
   return (
     <Background>
       <View style={styles.header}>
@@ -69,19 +65,25 @@ export function Home() {
         setCategory={handleCategorySelect}
       />
 
-      <ListHeader title={'Partidas agendadas'} subtitle={'total: 6'} />
+      {loading ? (
+        <Load />
+      ) : (
+        <>
+          <ListHeader title={'Partidas agendadas'} subtitle={'total: 6'} />
 
-      <FlatList
-        data={appointments}
-        keyExtractor={(item) => item.id}
-        style={styles.matches}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <Appointment data={item} onPress={handleAppointmentDetails} />
-        )}
-        ItemSeparatorComponent={() => <ListDivider />}
-        contentContainerStyle={{ paddingBottom: 69 }}
-      />
+          <FlatList
+            data={appointments}
+            keyExtractor={(item) => item.id}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Appointment data={item} onPress={handleAppointmentDetails} />
+            )}
+            ItemSeparatorComponent={() => <ListDivider />}
+            contentContainerStyle={{ paddingBottom: 69 }}
+          />
+        </>
+      )}
     </Background>
   );
 }
